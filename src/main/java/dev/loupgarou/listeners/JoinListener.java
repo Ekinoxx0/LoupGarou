4ce2677fe51133317fe22ccaaa23d0e319c765ab
Ekinoxx <fadow0711@gmail.com>
2020-03-29 22:33:27 +0200

    Changed link

--- src/main/java/dev/loupgarou/listeners/JoinListener.java
+++ src/main/java/dev/loupgarou/listeners/JoinListener.java
@@ -50,7 +50,7 @@ public class JoinListener implements Listener{
 			}
 		p.setFoodLevel(20);
 		if(e.getJoinMessage() == null || !e.getJoinMessage().equals("joinall"))
-			p.getPlayer().setResourcePack("http://leomelki.fr/mcgames/ressourcepacks/v29/loup_garou.zip");
+			p.getPlayer().setResourcePack("https://github.com/Ekinoxx0/LoupGarouRessourcePack/raw/master/loup_garou.zip");
 		else {
 			LGPlayer lgp = LGPlayer.thePlayer(e.getPlayer());
 			lgp.setDead(false);
