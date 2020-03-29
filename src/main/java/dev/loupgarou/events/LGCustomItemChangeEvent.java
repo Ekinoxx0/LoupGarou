75a527f560501573b61271af05290e92c853fe5b
Ekinoxx <fadow0711@gmail.com>
2020-03-29 18:28:29 +0200

    Oopsie, javadoc need to be class related, not constructor

--- src/main/java/dev/loupgarou/events/LGCustomItemChangeEvent.java
+++ src/main/java/dev/loupgarou/events/LGCustomItemChangeEvent.java
@@ -6,19 +6,20 @@ import dev.loupgarou.classes.LGCustomItems.LGCustomItemsConstraints;
 import dev.loupgarou.classes.LGGame;
 import dev.loupgarou.classes.LGPlayer;
 import lombok.Getter;
+import lombok.NonNull;
 
+/**
+ * Called each time we need special items for a specific player
+ * @param game The current game of the player
+ * @param player The player who need a special item
+ * @param constraints The list of contraints like
+ * @see LGCustomItemsConstraints
+ */
 public class LGCustomItemChangeEvent extends LGEvent {
 	@Getter private final LGPlayer player;
 	@Getter private final List<LGCustomItemsConstraints> constraints;
 	
-	/**
-	 * Called each time we need special items for a specific player
-	 * @param game The current game of the player
-	 * @param player The player who need a special item
-	 * @param constraints The list of contraints like
-	 * @see LGCustomItemsConstraints
-	 */
-	public LGCustomItemChangeEvent(LGGame game, LGPlayer player, List<LGCustomItemsConstraints> constraints) {
+	public LGCustomItemChangeEvent(@NonNull LGGame game, @NonNull LGPlayer player, @NonNull List<LGCustomItemsConstraints> constraints) {
 		super(game);
 		this.player = player;
 		this.constraints = constraints;
