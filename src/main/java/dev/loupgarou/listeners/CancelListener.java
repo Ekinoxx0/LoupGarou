de2c512515848ad1a811e5105c8931bfebef7f6e
Ekinoxx <fadow0711@gmail.com>
2020-03-29 18:20:05 +0200

    CancelListener modif
    Refactoring
    Removed useless comments
    Added optimization events

--- src/main/java/dev/loupgarou/listeners/CancelListener.java
+++ src/main/java/dev/loupgarou/listeners/CancelListener.java
@@ -1,18 +1,40 @@
 package dev.loupgarou.listeners;
 
 import org.bukkit.GameMode;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockBurnEvent;
+import org.bukkit.event.block.BlockDamageEvent;
+import org.bukkit.event.block.BlockExplodeEvent;
+import org.bukkit.event.block.BlockFadeEvent;
+import org.bukkit.event.block.BlockFormEvent;
+import org.bukkit.event.block.BlockFromToEvent;
+import org.bukkit.event.block.BlockGrowEvent;
+import org.bukkit.event.block.BlockIgniteEvent;
+import org.bukkit.event.block.BlockPhysicsEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.entity.EntityInteractEvent;
+import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.event.player.PlayerItemDamageEvent;
+import org.bukkit.event.player.PlayerItemHeldEvent;
+import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.event.player.PlayerSwapHandItemsEvent;
@@ -29,7 +51,25 @@ public class CancelListener implements Listener{
     }
     
 	@EventHandler
-	public void onBreak(BlockBreakEvent e) {
+	public void onBlockBreak(BlockBreakEvent e) {
+		if(e.getPlayer().getGameMode() == GameMode.CREATIVE) return;
+		e.setCancelled(true);
+	}
+
+	@EventHandler
+	public void onBlockDamage(BlockDamageEvent e) {
+		if(e.getPlayer().getGameMode() == GameMode.CREATIVE) return;
+		e.setCancelled(true);
+	}
+
+	@EventHandler
+	public void onBlockPlace(BlockPlaceEvent e) {
+		if(e.getPlayer().getGameMode() == GameMode.CREATIVE) return;
+		e.setCancelled(true);
+	}
+
+	@EventHandler
+	public void onPlayerItemHeld(PlayerItemHeldEvent e) {
 		if(e.getPlayer().getGameMode() == GameMode.CREATIVE) return;
 		e.setCancelled(true);
 	}
@@ -40,7 +80,7 @@ public class CancelListener implements Listener{
 	}
 	
 	@EventHandler
-	public void onMove(PlayerMoveEvent e) {
+	public void onPlayerMove(PlayerMoveEvent e) {
 		if(e.getPlayer().getGameMode() == GameMode.CREATIVE) return;
 		LGPlayer lgp = LGPlayer.thePlayer(e.getPlayer());
 		if(lgp.getGame() != null && lgp.getGame().isStarted() && e.getFrom().distanceSquared(e.getTo()) > 0.001)
@@ -48,48 +88,151 @@ public class CancelListener implements Listener{
 	}
 	
 	@EventHandler
-	public void onDamage(EntityDamageEvent e) {
+	public void onEntityDamage(EntityDamageEvent e) {
 		e.setCancelled(true);
 	}
 	
 	@EventHandler
-	public void onFood(FoodLevelChangeEvent e) {
+	public void onFoodLevelChange(FoodLevelChangeEvent e) {
 		e.setFoodLevel(20);
 	}
 	
 	@EventHandler
-	public void onRespawn(PlayerRespawnEvent e) {
+	public void onPlayerRespawn(PlayerRespawnEvent e) {
 		e.setRespawnLocation(e.getPlayer().getLocation());
 	}
 	
 	@EventHandler
-	public void onRespawn(PlayerDeathEvent e) {
-		e.setDeathMessage("");
+	public void onPlayerDeath(PlayerDeathEvent e) {
+		e.setDeathMessage(null);
+		e.setDroppedExp(0);
 		e.setKeepInventory(true);
+		e.setKeepLevel(true);
 	}
 	
 	@EventHandler
 	public void onEntitySpawn(EntitySpawnEvent e) {
 		e.setCancelled(true);
-		//ERREUR : LE LGN PEUT NE PAS AVOIR SON MENU SI IL A LE CHAT OUVERT PAR EX..AVOIR.
-		//SI QQN VOIT PLUS SON PERSO -> sneak
 	}
 	
 	@EventHandler
-	public void onDrop(PlayerDropItemEvent e) {
+	public void onPlayerDropItem(PlayerDropItemEvent e) {
 		if(e.getPlayer().getGameMode() == GameMode.CREATIVE) return;
 		e.setCancelled(true);
 	}
+	
 	@EventHandler
-	public void onClickInventory(InventoryClickEvent e) {
+	public void onInventoryClick(InventoryClickEvent e) {
 		if(e.getWhoClicked().getGameMode() == GameMode.CREATIVE) return;
-		if(LGPlayer.thePlayer((Player)e.getWhoClicked()).getGame() != null)
-			e.setCancelled(true);
+		e.setCancelled(true);
 	}
+	
 	@EventHandler
-	public void onClickInventory(PlayerSwapHandItemsEvent e) {
+	public void onPlayerSwapHandItems(PlayerSwapHandItemsEvent e) {
 		if(e.getPlayer().getGameMode() == GameMode.CREATIVE) return;
-		if(LGPlayer.thePlayer(e.getPlayer()).getGame() != null)
-			e.setCancelled(true);
+		e.setCancelled(true);
+	}
+
+	@EventHandler
+	public void onCraftItem(CraftItemEvent e) {
+		if(e.getWhoClicked().getGameMode() == GameMode.CREATIVE) return;
+		e.setCancelled(true);
+	}
+
+	@EventHandler
+	public void onPlayerInteractAtEntity(PlayerInteractAtEntityEvent e) {
+		if(e.getPlayer().getGameMode() == GameMode.CREATIVE) return;
+		e.setCancelled(true);
+	}
+
+	@EventHandler
+	public void onPlayerInteractEntity(PlayerInteractEntityEvent e) {
+		if(e.getPlayer().getGameMode() == GameMode.CREATIVE) return;
+		e.setCancelled(true);
+	}
+	
+	@EventHandler
+	public void onProjectileLaunch(ProjectileLaunchEvent e) {
+		e.setCancelled(true);
+	}
+
+	@EventHandler
+	public void onPlayerItemConsume(PlayerItemConsumeEvent e) {
+		e.setCancelled(true);
+	}
+
+	@EventHandler
+	public void onPlayerItemDamage(PlayerItemDamageEvent e) {
+		e.setCancelled(true);
+	}
+
+	@EventHandler
+	public void onBlockExplode(BlockExplodeEvent e) {
+		e.setCancelled(true);
+	}
+
+	@EventHandler
+	public void onEntityExplode(EntityExplodeEvent e) {
+		e.setCancelled(true);
+	}
+
+	@EventHandler
+	public void onPlayerKick(PlayerKickEvent e) {
+		e.setLeaveMessage(null);
+	}
+
+	@EventHandler
+	public void onBlockIgnite(BlockIgniteEvent e) {
+		e.setCancelled(true);
+	}
+
+	@EventHandler
+	public void onBlockBurn(BlockBurnEvent e) {
+		e.setCancelled(true);
+	}
+
+	@EventHandler
+	public void onBlockForm(BlockFormEvent e) {
+		e.setCancelled(true);
+	}
+
+	@EventHandler
+	public void onBlockFade(BlockFadeEvent e) {
+		e.setCancelled(true);
+	}
+
+	@EventHandler
+	public void onBlockGrow(BlockGrowEvent e) {
+		e.setCancelled(true);
+	}
+
+	@EventHandler
+	public void onBlockFromTo(BlockFromToEvent e) {
+		e.setCancelled(true);
+	}
+
+	@EventHandler
+	public void onBlockPhysics(BlockPhysicsEvent e) {
+		e.setCancelled(true);
+	}
+
+	@EventHandler
+	public void onEntityPickupItem(EntityPickupItemEvent e) {
+		e.setCancelled(true);
+	}
+
+	@EventHandler
+	public void onCreatureSpawn(CreatureSpawnEvent e) {
+		e.setCancelled(true);
+	}
+
+	@EventHandler
+	public void onProjectileHit(ProjectileHitEvent e) {
+		e.getEntity().remove();
+	}
+
+	@EventHandler
+	public void onEntityInteract(EntityInteractEvent e) {
+		e.setCancelled(true);
 	}
 }
