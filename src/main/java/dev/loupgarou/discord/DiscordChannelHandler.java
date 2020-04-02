10e31ff95d04216e63d38014630c1bbb3d8ac66b
Ekinoxx <fadow0711@gmail.com>
2020-04-02 21:35:45 +0200

    Did an oopsie on token again !

--- src/main/java/dev/loupgarou/discord/DiscordChannelHandler.java
+++ src/main/java/dev/loupgarou/discord/DiscordChannelHandler.java
@@ -4,39 +4,60 @@ import java.util.concurrent.TimeUnit;
 
 import dev.loupgarou.MainLg;
 import dev.loupgarou.classes.LGGame;
+import lombok.NonNull;
 import net.dv8tion.jda.api.entities.Invite;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.VoiceChannel;
 
 public class DiscordChannelHandler {
-	
+
+	private final DiscordManager discord;
 	private final LGGame game;
 	private VoiceChannel voice;
 	private Invite invite;
 	
-	public DiscordChannelHandler(LGGame game) {
+	private boolean isChannelMuted;
+	
+	public DiscordChannelHandler(@NonNull LGGame game) {
 		this.game = game;
+		this.discord = MainLg.getInstance().getDiscord();
 		
-		DiscordManager discord = MainLg.getInstance().getDiscord();
 		discord.getVoices().createVoiceChannel("LG - " + game.getOwner().getName()).setUserlimit(game.getMaxPlayers())
 			.queue((voice) -> {
 				this.voice = voice;
+				discord.register(this);
+				
 				voice.createInvite()
 					 .setMaxAge(90L, TimeUnit.MINUTES)
 					 .setMaxUses(100)
 					 .queue((invite) -> {
 						 this.invite = invite;
-						 //TODO
-						 invite.getUrl();
+						 game.broadcastMessage("§2Création du salon finalisé, rejoignez le discord via : " + invite.getUrl());
+					 },
+					 (failure) -> {
+						 this.invite = null;
+						 game.broadcastMessage("§cSalon créé, mais une erreur est survenue lors de la création de l'invitation...");
+						 failure.printStackTrace();
 					 });
+			},
+			(failure) -> {
+				game.broadcastMessage("§cUne erreur est survenue sur lors de la création du salon discord...");
+				failure.printStackTrace();
+				this.destroy();
 			});
 	}
 	
 	public void muteChannel(boolean mute) {
 		if(this.voice == null) return;
+		if(this.isChannelMuted == mute) return;
+		this.isChannelMuted = mute;
 		
 		for(Member member : this.voice.getMembers())
 			member.mute(mute).queue();
+	}
+	
+	public boolean isValid() {
+		return this.game != null && !this.game.isEnded() && this.voice != null;
 	}
 	
 	public void destroy() {
@@ -44,6 +65,12 @@ public class DiscordChannelHandler {
 			invite.delete().queue();
 		if(voice != null)
 			voice.delete().queue();
+		if(game != null)
+			game.broadcastMessage("§6Destruction de la liaison Discord...");
+		
+		this.invite = null;
+		this.voice = null;
+		MainLg.getInstance().getDiscord().unregister(this);
 	}
 	
 }
--- src/main/java/dev/loupgarou/discord/DiscordManager.java
+++ src/main/java/dev/loupgarou/discord/DiscordManager.java
@@ -3,20 +3,22 @@ package dev.loupgarou.discord;
 import java.io.InputStream;
 import java.io.StringWriter;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.bukkit.craftbukkit.libs.org.apache.commons.io.IOUtils;
 
 import dev.loupgarou.MainLg;
 import dev.loupgarou.classes.LGPlayer;
 import lombok.Getter;
+import lombok.NonNull;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Category;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.VoiceChannel;
-import net.dv8tion.jda.api.events.guild.voice.GuildVoiceJoinEvent;
-import net.dv8tion.jda.api.events.guild.voice.GuildVoiceLeaveEvent;
+import net.dv8tion.jda.api.events.guild.voice.GuildVoiceUpdateEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class DiscordManager extends ListenerAdapter {
@@ -37,8 +39,10 @@ public class DiscordManager extends ListenerAdapter {
 	
 	@Getter private final JDA jda;
 	@Getter private final Category voices;
+	private final List<DiscordChannelHandler> handlers;
 	
 	public DiscordManager(MainLg main) throws Exception {
+		this.handlers = new ArrayList<DiscordChannelHandler>();
 		this.jda = new JDABuilder(TOKEN).build();
 	    this.jda.addEventListener(this);
 	    try {
@@ -48,62 +52,83 @@ public class DiscordManager extends ListenerAdapter {
 		}
 	    
 	    this.voices = this.jda.getCategoryById(CATEGORY_ID);
-	    if(this.voices == null) {
+	    if(this.voices == null)
 	    	throw new Exception("Section id is unknown.");
-	    }
 
 		for(VoiceChannel voice : this.voices.getVoiceChannels())
 			voice.delete().queue();
 	}
 	
 	@Override
-    public void onGuildVoiceJoin(GuildVoiceJoinEvent e) {
-		//TODO
-    }
-	
-	@Override
-    public void onGuildVoiceLeave(GuildVoiceLeaveEvent e) {
+    public void onGuildVoiceUpdate(GuildVoiceUpdateEvent e) {
+		LGPlayer lgp = this.get(e.getEntity());
+		boolean leftGameChannel = this.voices.getChannels().contains(e.getChannelLeft());
 		
+		if(e.getChannelJoined() == null) {
+			//Disconnected
+			if(lgp == null) return;
+			lgp.sendMessage(MainLg.getPrefix() + "§cDéconnecté du discord...");
+			return;
+		}
+		
+		if(this.voices.getChannels().contains(e.getChannelJoined())) {
+			if(leftGameChannel) {
+				//Moved from a game to a game
+			} else {
+				//Joined a game
+			}
+		} else if(leftGameChannel) {
+			//Moved from game to random
+			if(lgp == null) return;
+			lgp.sendMessage(MainLg.getPrefix() + "§cVous avez quitté les salons de jeux Loup Garou.");
+		} else {
+			//Moved from random to random
+		}
     }
 	
-	public LGPlayer get(Member member) {
-		if(member == null) return null;
+	//Methods
+	
+	public void register(@NonNull DiscordChannelHandler handler) {
+		if(handler.isValid())
+			this.handlers.add(handler);
+		else
+			MainLg.debug("Tried to register an invalid discord handler");
+	}
+	
+	public void unregister(@NonNull DiscordChannelHandler handler) {
+		this.handlers.remove(handler);
+		if(handler.isValid())
+			handler.destroy();
+	}
+	
+	public LGPlayer get(@NonNull Member member) {
 		if(this.jda == null) return null;
 		if(this.voices == null) return null;
 		
 		for(LGPlayer lgp : LGPlayer.all()) {
-			if(member.getEffectiveName().toLowerCase().contains(lgp.getName().toLowerCase())) {
+			if(member.getEffectiveName().toLowerCase().contains(lgp.getName().toLowerCase()))
 				return lgp;
-			}
 			
-			for(Role r : member.getRoles()) {
-				if(r.getName().equals("!" + lgp.getName())) {
+			for(Role r : member.getRoles())
+				if(r.getName().equals("!" + lgp.getName()))
 					return lgp;
-				}
-			}
 		}
 		
 		return null;
 	}
 	
-	public Member get(String playerName) {
-		if(playerName == null) return null;
+	public Member get(@NonNull String playerName) {
 		if(this.jda == null) return null;
 		if(this.voices == null) return null;
 		
-		for(Member m : this.voices.getMembers()) {
-			if(m.getEffectiveName().toLowerCase().contains(playerName.toLowerCase())) {
+		for(Member m : this.voices.getMembers())
+			if(m.getEffectiveName().toLowerCase().contains(playerName.toLowerCase()))
 				return m;
-			}
-		}
 
-		for(Member m : this.voices.getMembers()) {
-			for(Role r : m.getRoles()) {
-				if(r.getName().equals("!" + playerName)) {
+		for(Member m : this.voices.getMembers())
+			for(Role r : m.getRoles())
+				if(r.getName().equals("!" + playerName))
 					return m;
-				}
-			}
-		}
 		
 		return null;
 	}
